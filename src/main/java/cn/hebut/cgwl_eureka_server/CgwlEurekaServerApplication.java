cfe544834297908ab077464433a2cf81b07e1ab3
jszhang <jszhang@x230>
2021-04-22 23:36:26 +0800

    first commit

---
+++ src/main/java/cn/hebut/cgwl_eureka_server/CgwlEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package cn.hebut.cgwl_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgwlEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgwlEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/hebut/cgwl_eureka_server/CgwlEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hebut.cgwl_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgwlEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
